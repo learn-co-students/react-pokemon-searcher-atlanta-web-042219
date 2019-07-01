aa580ca7847c20e44f221dd279bc7021e16c81cc
Han Xu <hanxu27@gmail.com>
2019-07-01 19:29:15 -0400

    Done.

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -4,17 +4,44 @@ import PokemonForm from './PokemonForm'
 import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
+const API = `http://localhost:3000/pokemon`
+
 class PokemonPage extends React.Component {
+  state = {
+    pokemons: [],
+    filteredMons: null,
+    update: false
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(res => res.json())
+      .then(data => this.setState({ pokemons: data, update: false }))
+  }
+
+  searchPokemon = (e, { value }) => {
+    this.setState({ filteredMons: this.state.pokemons.filter(pokemon => pokemon.name.includes(value)) })
+  }
+
+  formUpdate = () => {
+    this.setState({ update: true })
+  }
+
+  addPokemon = (pokemon) => {
+    this.setState({ pokemons: [...this.state.pokemons, pokemon] })
+  }
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={
+          _.debounce(this.searchPokemon, 500)}
+          showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={this.state.filteredMons ? this.state.filteredMons : this.state.pokemons} />
         <br />
-        <PokemonForm />
+        <PokemonForm update={this.formUpdate} addPokemon={this.addPokemon} />
       </div>
     )
   }
